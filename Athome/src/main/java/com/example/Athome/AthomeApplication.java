738512d64da4df0e2dbdcfecef334fd02df871d7
jiyeonhae <bjy51188@gmail.com>
2022-11-01 21:03:47 +0900

    1101H

---
+++ Athome/src/main/java/com/example/Athome/AthomeApplication.java
@@ -0,0 +1,14 @@
+package com.example.Athome;
+//192.168.45.218 ip
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthomeApplication.class, args);
+	}
+
+}
---
+++ Athome/src/test/java/com/example/Athome/AthomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Athome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
