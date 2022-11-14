2bde336912874640245f2acbd7988fc145c2a3fd
jiyeonhae <bjy51188@gmail.com>
2022-11-14 22:41:46 +0900

    1114d

---
+++ Practice/src/main/java/com/ProductP/src/main/java/com/example/ProductP/ProductPApplication.java
@@ -0,0 +1,13 @@
+package com.ProductP.src.main.java.com.example.ProductP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPApplication.class, args);
+	}
+
+}
---
+++ Practice/src/main/java/com/ProductP/src/test/java/com/example/ProductP/ProductPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
